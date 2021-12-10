48ce0b978fd7086da0ab596fb25b6975a7b84fba
philimaliste <pcoumbassa@gmail.com>
2021-12-10 09:37:40 +0100

    make it better

--- app/atotiwatcher.py
+++ app/atotiwatcher.py
@@ -13,12 +13,13 @@ class AtotiWatcher(FileSystemEventHandler):
         try:
             dataprocessor = DataProcessor()
             src_path = event.src_path
-            if "EDEN.csv" in src_path[-10:]:
-                var_df = dataprocessor.read_var_file(src_path)
-                self.session.tables["Var"].load_pandas(var_df)
+            print(src_path[-10:])
             if "ScenarioDate" in src_path:
                 explain_df = dataprocessor.read_explain_file(src_path)
                 self.session.tables["Explain"].load_pandas(explain_df)
+            else:
+                var_df = dataprocessor.read_var_file(src_path)
+                self.session.tables["Var"].load_pandas(var_df)
         except Exception as error:
             print(error)
 
@@ -27,9 +28,9 @@ class AtotiWatcher(FileSystemEventHandler):
             dataprocessor = DataProcessor()
             src_path = event.src_path
             print("file deleted", src_path)
-            if "EDEN.csv" in src_path[-10:]:
-                self.session.tables["Var"].drop({"Pathfile": src_path})
             if "ScenarioDate" in src_path:
                 self.session.tables["Explain"].drop({"Pathfile": src_path})
+            else:
+                self.session.tables["Var"].drop({"Pathfile": src_path})
         except Exception as error:
             print(error)
